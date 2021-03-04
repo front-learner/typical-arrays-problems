4fc1a5b7bf5ec748a50631d4be24e3f70347b8d8
front-learner <79720458+front-learner@users.noreply.github.com>
2021-03-04 13:03:08 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let minValue = array[0];
+  for (let i = 1; i < array.length; i++){
+      if (array[i] < minValue) {
+        minValue = array[i];
+      } else continue;
+  }
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let maxValue = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maxValue) {
+      maxValue = array[i];
+    } else continue;
+  }
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = array[i] + sum;
+  }
+  let avgValue = sum / array.length;
+  return avgValue;
 }
